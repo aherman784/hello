7234d3d5f3dc9a939a4edc014558a78ee55f9c16
aherman784 <alexherman987@gmail.com>
2020-08-22 17:09:16 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Alex Herman
+ *         aherman7@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Alex Herman!\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
